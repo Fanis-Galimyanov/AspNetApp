5b95a6553afb2d82a02a8247483962f78d1b0438
Fanis-Galimyanov <Saba1986Biganay1986Satish1986Mamalay>
2022-04-25 18:36:09 +0300

    25.04.2022

---
+++ AspNetApp/Migrations/20220425132823_Order.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetApp.Migrations
+{
+    public partial class Order : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    surname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    adress = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    orderTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    orderID = table.Column<int>(type: "int", nullable: false),
+                    CarID = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderDetails", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_orderDetails_Car_CarID",
+                        column: x => x.CarID,
+                        principalTable: "Car",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orderDetails_Order_orderID",
+                        column: x => x.orderID,
+                        principalTable: "Order",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderDetails_CarID",
+                table: "orderDetails",
+                column: "CarID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderDetails_orderID",
+                table: "orderDetails",
+                column: "orderID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
