4114cbbe9a48ef5b9c1aec49a6f7a7442c3d460e
Fanis Galimyanov <fanisgalimyanov@gmail.com>
2022-05-12 20:30:24 +0300

    Search

--- AspNetApp/Controllers/HomeController.cs
+++ AspNetApp/Controllers/HomeController.cs
@@ -1,15 +1,34 @@
 ﻿using AspNetApp.interfaces;
 using AspNetApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IAllCars _allCars;
-        public HomeController(IAllCars iAllCars)
+        private readonly AppDBContent _db;
+        public HomeController(IAllCars iAllCars, AppDBContent db)
         {
             _allCars = iAllCars;
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string Empsearch)
+        {
+            ViewData["Getemployeedetails"] = Empsearch;
+            var empquery = from x in _db.Car select x;
+            if(!String.IsNullOrEmpty(Empsearch))
+            {
+                empquery =  empquery.Where(x=>x.name.Contains(Empsearch) || x.Category.categoryName.Contains(Empsearch));
+            }
+            var homeCars = new HomeViewModel
+            {
+                favCars = await empquery.AsNoTracking().ToListAsync()
+            };
+            return View(homeCars);         
         }
         public ViewResult Index()
         {
