0c3d163a0354e718ea0ab646344afeee206a0ca3
Fanis-Galimyanov <Saba1986Biganay1986Satish1986Mamalay>
2022-05-12 17:23:01 +0300

    22

---
+++ emailNew/emailNew/Program.cs
@@ -0,0 +1,25 @@
+using emailNew;
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo("DataProtectionKeys"));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<Service>();
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ emailNew/emailNew/Service.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace emailNew
+{
+    public class Service
+    {
+        private readonly ILogger<Service> logger;
+        public Service(ILogger<Service> logger)
+        {
+            this.logger = logger;
+        }
+        public void SendEmail()
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.IsBodyHtml = true;
+                message.From = new MailAddress("admin@itpark.com", "ITPark");
+                message.To.Add("fanisga@yandex.ru");
+                message.Subject = "Сообщение от System.Net.Mail";
+                //message.Body = "Сообщение от System.Net.Mail";
+
+                var plainView = AlternateView.CreateAlternateViewFromString("Просто текст. Текст 1", null, "text/plain");
+                var htmlView = AlternateView.CreateAlternateViewFromString("Текскт рядом с рисунком..<img src=cid:companylogo>" +
+                    "<img src=cid:dtId> <img src=cid:companylogo> <img src=cid:dtId> <img src=cid:companylogo>",
+                                                                            null, "text/html");
+
+                LinkedResource logo = new LinkedResource("wwwroot/img/s.jpg");
+                LinkedResource dt = new LinkedResource("wwwroot/img/ms.jpg");
+                logo.ContentId = "companylogo";
+                dt.ContentId = "dtId";
+
+                htmlView.LinkedResources.Add(logo);
+                htmlView.LinkedResources.Add(dt);
+
+                message.AlternateViews.Add(plainView);
+                message.AlternateViews.Add(htmlView);
+
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com"))
+                {
+                    client.Credentials = new NetworkCredential("soportmedimist@gmail.com", "kazan_tatarstan");
+                    client.Port = 587;
+                    client.EnableSsl = true;
+
+                    client.Send(message);
+                    logger.LogInformation("Сообщение отправлено успешно!");
+                }
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.GetBaseException().Message);
+            }
+
+        }
+    }
+}
